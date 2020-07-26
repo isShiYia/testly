8e9372dec1d8d4496f7cb0df71e59cf6ab1faf41
apple <2644108676@qq.com>
2020-07-26 17:24:17 +0800

    Add  a lua test

---
+++ test1.lua
@@ -0,0 +1,21 @@
+-- local tab = {}
+-- local function tab.add(sequencer.params[1],sequencer.params[2])
+-- 	if(sequencer.params[1] > sequencer.params[2]) then
+-- 		return tonumber(sequencer.params[1]) - tonumber(sequencer.params[2])
+-- 	else
+-- 		return tonumber(sequencer.params[1]) - tonumber(sequencer.params[2])
+-- 	end
+-- end
+-- return tab
+
+
+-- add(5,3)
+
+local ta1 = {}
+function tab.add(sequence)
+	return sequence.params1+sequence.params2
+end
+return ta1
+
+
+print("xiali123")
